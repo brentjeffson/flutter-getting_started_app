b27be9d5fa2ec765a09b2c6569b5185753643aa0
Brent Jeffson Florendo <brentjeffson@gmail.com>
2020-04-10 13:08:57 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Getting Started App",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Gotu',
+            letterSpacing: 4.0,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
+
